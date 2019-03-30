3379107cbe9e459fa741033c876feb74ad77553e
Recep Mert ASLAN <recoo_1995@hotmail.com>
2019-03-30 17:04:29 +0300

    UserProfil Controller Eklendi
    Bu controller kullanicinin kendi sayfasini ve takaslarini gosterecegi yer ve bunlari guncelleyebilecek

---
+++ RitminiDegis/GroupX-master/GroupX-master/Takas.MvcWebUI/Controllers/UserProfilController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Takas.MvcWebUI.Controllers
+{
+	/// <summary>
+	/// Kullanici Kendi takas listelerini goruntuleyip bilgielrini guncelleyebilecek
+	/// </summary>
+    public class UserProfilController : Controller
+    {
+        // GET: UserProfil
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
