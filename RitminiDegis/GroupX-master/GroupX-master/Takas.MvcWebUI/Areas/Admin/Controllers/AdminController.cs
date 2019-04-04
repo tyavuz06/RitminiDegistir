4ddd44796934774b01c0168da166292cfdc7ffb7
Recep Mert ASLAN <recoo_1995@hotmail.com>
2019-04-04 11:25:17 +0300

    Admin Panel e Devam edildi
    Admin Panel islemlerine devam edildi

--- RitminiDegis/GroupX-master/GroupX-master/Takas.MvcWebUI/Areas/Admin/Controllers/AdminController.cs
+++ RitminiDegis/GroupX-master/GroupX-master/Takas.MvcWebUI/Areas/Admin/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.MappingViews;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Takas.Entities.Concrete;
 
 namespace Takas.MvcWebUI.Areas.Admin.Controllers
 {
@@ -11,7 +13,46 @@ namespace Takas.MvcWebUI.Areas.Admin.Controllers
         // GET: Admin/Admin
         public ActionResult Index()
         {
-            return View();
+			//todo Bu sayfada bir model gidcek bu model de User Class imizi gonderecek. Veritabanindaki tum kullanicilari buraya gonderecegiz. WebApi den gelecek buraya degerler. Admin Panel aslinda Web Api ile calismasa da olur mu acaba bunu bir dusunelim.
+			return View();
         }
-    }
+
+		public ActionResult UserList()
+		{
+			//todo Bu sayfada bir model gidcek bu model de User Class imizi gonderecek. Veritabanindaki tum kullanicilari buraya gonderecegiz. WebApi den gelecek buraya degerler.
+			return View("_PartialPageAdminPanelUserList");
+		}
+
+		[HttpGet]
+		public ActionResult AddUser()
+		{
+			return View();
+		}
+
+		[HttpPut]
+		public ActionResult Update(User user)
+		{
+			//Todo Web Api uzerinden islem yapacagiz
+			return View();
+		}
+
+		[HttpDelete]
+		public ActionResult Delete(int ID)
+		{
+			//Todo Web Api uzerinden islem yapacagiz
+			return View();
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+	}
 }
