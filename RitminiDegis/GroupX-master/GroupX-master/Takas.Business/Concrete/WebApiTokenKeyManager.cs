00a190c0986a3337659573fab07a014621aaa1f1
Recep Mert ASLAN <recoo_1995@hotmail.com>
2019-04-06 01:18:43 +0300

    WebApi Token Islemleri
    Web Api de WebApiTokenKey olarak tokenlar olusturoyurz.

---
+++ RitminiDegis/GroupX-master/GroupX-master/Takas.Business/Concrete/WebApiTokenKeyManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Takas.Business.Abstract;
+using Takas.DataAccess.Abstract;
+
+namespace Takas.Business.Concrete
+{
+	public class WebApiTokenKeyManager :IWebApiTokenKeyService
+	{
+		private IWebApiTokenKeyDal _webApiTokenKeyDal;
+
+		public WebApiTokenKeyManager(IWebApiTokenKeyDal webApiTokenKeyDal)
+		{
+			_webApiTokenKeyDal = webApiTokenKeyDal;
+		}
+	}
+}
---
+++ RitminiDegis/GroupX-master/GroupX-master/Takas.Common/Entities/Concrete/WebApiTokenKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Takas.Common.Entities.Abstract;
+
+namespace Takas.Common.Entities.Concrete
+{
+	public class WebApiTokenKey : IEntity
+	{
+		public int ID { get; set; }
+
+		public string UserName { get; set; }
+
+		public string UserKey { get; set; }
+
+		public string UserRole { get; set; }
+	}
+}
