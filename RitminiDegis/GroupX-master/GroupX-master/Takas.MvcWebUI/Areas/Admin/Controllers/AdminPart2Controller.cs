fcb54a7710762cf156bbde4feb12ff1a5cc6be75
Tansu Ağgöz <tansu.aggoz@gmail.com>
2019-04-06 21:26:21 +0300

    admin sayfaında kategori işlemleri

---
+++ RitminiDegis/GroupX-master/GroupX-master/Takas.MvcWebUI/Areas/Admin/Controllers/AdminPart2Controller.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Takas.Common.Entities.Concrete;
+
+
+namespace Takas.MvcWebUI.Areas.Admin.Controllers
+{
+    public class AdminPart2Controller : Controller
+    {
+        // GET: Admin/AdminPart2
+        public ActionResult CategoryList()
+        {
+            return View("_PartialPageAdminPanelCategoryList");
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddCategory(Category category)
+        {
+            //Todo Burada Kullanici ekleme islemi yapilacak WebApi uzerinden.	
+            //ToDo viewbag ile deger gonderelim bu sayfamiza kullanici kayit olduktan sonra ekrana popup acip kullanci basarili bir sekilde kaydoldu diyebiliriz.
+            return View();
+        }
+
+        public ActionResult Update(int id)
+        {
+            //Todo Web Api uzerinden islem yapacagiz
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                //Todo WebApi ye gonder Kayit Islemini Yapsin
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            // Todo Web Api uzerinden islem yapacagiz
+            //todo Gelen id ye gore 
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
