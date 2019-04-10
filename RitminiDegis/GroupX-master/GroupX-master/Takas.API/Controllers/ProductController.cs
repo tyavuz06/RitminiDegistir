6f3963e2d00781d2287c7d026bbf379af5629133
TURGUT YAVUZ ÜNLÜ <turgut_yavuz_06@hotmail.com>
2019-04-10 16:45:28 +0300

    ProductController

---
+++ RitminiDegis/GroupX-master/GroupX-master/Takas.API/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Takas.API.Controllers
+{
+    [EnableCors(origins: "http://localhost:50903/", headers: "*", methods: "*")]
+    public class ProductController : ApiController
+    {
+        [HttpPost]
+        [Route("api/Product/AddProduct")]
+        public object AddProduct()
+        {
+            return null;
+        }
+    }
+}
